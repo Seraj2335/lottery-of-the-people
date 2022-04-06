d79463e55999846e8242ee8f37ab59b9fd3744f4
Seraj ahmad <kseraj96@gmail.com>
2022-04-06 13:36:14 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors:{
+        primary:"#081f4e",
+        secondary:"#0083fd",
+        neutral:"#384d70"
+       }
+
+    },
+
+  },
+  plugins: [],
+}
