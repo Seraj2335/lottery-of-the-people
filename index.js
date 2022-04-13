4a86ae873471aee2652908e9fa1c81bcbc30d7e7
Seraj ahmad <kseraj96@gmail.com>
2022-04-13 11:32:33 +0530

    commit

--- index.js
+++ index.js
@@ -3,8 +3,11 @@
 const menuObject=document.querySelector('#menu');
 const listObject=document.querySelector('#ulList');
 
-menuObject.addEventListener("click",()=>{
-    if(listObject.contains('hidden')){
+menuObject.addEventListener("click",function(){
+
+   
+    
+    if(listObject.classList.contains('hidden')){
         listObject.classList.remove('hidden');
     }else{
         listObject.classList.add('hidden');
